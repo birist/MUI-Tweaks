1b9ede8d2902b12ea7245d7c13d16136dc731bd1
birist <84549605+birist@users.noreply.github.com>
2022-07-14 16:57:52 -0600

    Add files via upload
    Prototypes for
    -MUI resizing for always on display
    -MetasysAPI/MUI token refresh for always on display

---
+++ Chrome Extension MUI Resize/content.js
@@ -0,0 +1,27 @@
+document.getElementById("navigation").style = "visibility: hidden; min-width: 0px; width: 0px";
+
+document.getElementById("page-head").style = "visibility: hidden; height: 0px";
+
+document.getElementsByClassName("header-controls-bar")[0].style = "height: 0px; padding: 0px; visibility: hidden";
+
+document.getElementById("widget-1").children[0].children[0].style = "height: 0px";
+
+document.getElementById("widget-1").children[0].children[1].style = "height: 0px";
+
+document.getElementById("content-container").style = "top: 0px";
+
+document.getElementById("dashboard-page-list").style = "margin: 0px; width: 100%; height: 100%";
+
+document.getElementsByClassName("widget-placeholder")[0].style = "height: 100%; width: 100%";
+
+document.getElementsByClassName("graphic-controls-menu menu-slide-out")[0].style = "width: 0px";
+
+document.getElementsByClassName("floor-graphics-controls controls-slide-out")[0].style = "width: 0px";
+
+document.getElementsByClassName("svg")[0].style = "width: 100%";
+
+document.getElementById("widget-1").children[0].style = "visibility: hidden; height: 0px; padding: 0px";
+
+document.getElementsByClassName("svg-pan-zoom_viewport")[0].style = "transform: matrix(1,0,0,1,0,0)";
+
+alert("Hit F11 to make the browser fullscreen.");
